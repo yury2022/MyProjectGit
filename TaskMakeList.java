9ca7eee176f8a8bcbcf7eedb978fc67494b89e0f
Yury Kasmachou <mdm.bzkt@gmail.com>
2022-06-05 12:51:49 +0300

    Создал метод поиска min числа

--- TaskMakeList.java
+++ TaskMakeList.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TaskMakeList {
 
+    static void CreateList(int number0, int number1, int number2, int number3, int number4, int number5) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(number0);
+        numbers.add(number1);
+        numbers.add(number2);
+        numbers.add(number3);
+        numbers.add(number4);
+        numbers.add(number5);
+        System.out.println("Наименьше значение списка равно: " + Collections.min(numbers));
+    }
 }
